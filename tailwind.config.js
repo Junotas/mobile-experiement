3f746f2209a3cb0f81b4b588209a75a17a2fde83
Ludvig <ludvig.fendert@appliedtechnology.se>
2024-12-13 13:41:03 +0100

    changed from tailwind fluid to fluid tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,23 @@
 /** @type {import('tailwindcss').Config} */
-import fluid from 'tailwindcss-fluid';
+import fluid, { extract } from 'fluid-tailwind';
 
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
-  theme: {
-    extend: {},
+  content: {
+    files: [
+      "./index.html",
+      "./src/**/*.{js,jsx,ts,tsx}",
+    ],
+    extract, // Required for Fluid Tailwind
   },
-  plugins: [
-    fluid(),
-  ],
+  theme: {
+    screens: {
+      xs: '20rem', // Add custom screens (optional)
+    },
+    extend: {
+      fluid: ({ theme }) => ({
+        defaultScreens: ['20rem', theme('screens.lg')], // Customize default start/end breakpoints
+      }),
+    },
+  },
+  plugins: [fluid],
 };
